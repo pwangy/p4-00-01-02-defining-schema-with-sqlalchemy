18facda9fe503ce7e1cd656836101f18148780a7
pwangy <blipsandclicks@gmail.com>
2024-03-30 21:43:59 -0700

    complete codealong

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,14 @@
+# lib/sqlalchemy_sandbox.py
 #!/usr/bin/env python3
 
+#  imports
 from sqlalchemy import create_engine
-
 from sqlalchemy_sandbox import Student
+
+# data models
 
 engine = create_engine('sqlite:///students.db')
 
 if __name__ == '__main__':
+    Base.metadata.create_all(engine)
     import ipdb; ipdb.set_trace()
